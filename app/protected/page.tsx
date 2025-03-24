e7e1127494780d7abe2def23500860dbd9c4e2e9
Sebastian Pian <89761448+sebapian@users.noreply.github.com>
2025-03-24 23:34:48 +1300

    add button

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -2,6 +2,7 @@ import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
 import { createClient } from "@/utils/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
+import { ApprovalButton } from "@/components/approval-button";
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
@@ -28,6 +29,10 @@ export default async function ProtectedPage() {
         <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
           {JSON.stringify(user, null, 2)}
         </pre>
+      </div>
+      <div className="flex flex-col gap-4">
+        <h2 className="font-bold text-2xl">Create Approval</h2>
+        <ApprovalButton />
       </div>
       <div>
         <h2 className="font-bold text-2xl mb-4">Next steps</h2>
