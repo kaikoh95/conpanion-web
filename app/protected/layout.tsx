66b2fc18bf719717875d5a0190c0c6892d8575d9
Sebastian Pian <89761448+sebapian@users.noreply.github.com>
2025-04-13 21:52:42 +1200

    feat: Make sidebar mobile responsive (#12)
    * Add CSS for Sidebar
    
    * Update AppLayout.tsx
    
    * Can't compile so push changes
    
    * Update models.ts
    
    * Update TaskDrawer.tsx
    
    * Try it out
    
    * Revert "Can't compile so push changes"
    
    This reverts commit 6130e8615e7d25502a28ee22a1f9909e9b894cdb.
    
    * Revert "Update models.ts"
    
    This reverts commit c19360e082196a33c2715c31ea1d1fa5eaa46d8f.
    
    * Revert "Update TaskDrawer.tsx"
    
    This reverts commit 31d0b57f07d67af95539a44ddf3aad1b82f1e837.
    
    * Revert "Try it out"
    
    This reverts commit 2613c1b99b2c07cb5fa2c47e7750b2096842002a.

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,8 +1,6 @@
 'use client'
-import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import Sidebar from '../components/layout/Sidebar';
-import TopBar from '../components/layout/TopBar';
+import ClientLayout from './client-layout';
 import { useAuth } from "@/hooks/useAuth";
 
 export default function ProtectedLayout({
@@ -16,15 +14,5 @@ export default function ProtectedLayout({
     return redirect("/sign-in");
   }
 
-  return (
-    <div className="min-h-screen bg-background">
-      <Sidebar />
-      <TopBar />
-      <main className="transition-[padding-left] duration-300" style={{ paddingLeft: 'var(--sidebar-width)', paddingTop: '3.5rem' }}>
-        <div className="p-6">
-          {children}
-        </div>
-      </main>
-    </div>
-  );
+  return <ClientLayout>{children}</ClientLayout>;
 } 
